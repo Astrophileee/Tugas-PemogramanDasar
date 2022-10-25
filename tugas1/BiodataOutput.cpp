bc283a089334a751e674db47b1a08be26ef1d699
Sooyaaaaaaaaa <iqbalmaulanaasyari17@gmail.com>
2022-10-25 22:53:33 +0700

    Tugas Sebelum UTS

---
+++ tugas1/BiodataOutput.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main(){
+	system("cls");
+    char a = 177;
+    char b = 219;
+    cout << "\n\n\n\n\n\n\n\n\n\t\t\t\t\t\t     loading\n\n";
+    cout << "\t\t\t\t\t\t";
+    for (int i = 0;i <= 15;i++)
+        cout << a;
+    cout << "\r";
+    cout << "\t\t\t\t\t\t";
+    for (int i = 0;i <= 15;i++)
+    {
+        cout << b;
+        for (int j = 0;j <= 1e8;j++);
+    }
+    cin.get();
+	system("cls");
+	cout<<"\n\n\n\n\n\n\n\n\n\t\t\t\t\t\t========Hallo!=======\n\n";
+	cin.get();
+	system("cls");
+	cout<<"\n\n\n\n\n\n\n\n\n\t\t\t\t\t\t========Perkenalkan!=======\n\n";
+	cin.get();
+	system("cls");
+	cout << "\n\n\n\n\n\n\n\n\n\t\t\t\t==========================================================\n";
+    cout << "\t\t\t\t||\t\t            BIODATA           \t\t||\n";
+    cout << "\t\t\t\t|| Nama  \t\t\t: Iqbal Maulana Asyari \t||"<<endl;
+    cout << "\t\t\t\t|| Umur \t\t\t: \t\t\t||"<<endl;
+    cout << "\t\t\t\t|| Tanggal Lahir Anda \t\t: \t\t\t||"<<endl;
+    cout << "\t\t\t\t|| NPM \t\t\t\t: \t\t\t||"<<endl;
+    cout << "\t\t\t\t|| Kelas Anda  \t\t\t: \t\t\t||"<<endl;
+    cout << "\t\t\t\t|| Asal Sekolah    \t\t: \t\t\t||"<<endl;
+    cout << "\t\t\t\t==========================================================\n";
+	cin.get();
+	system("cls");
+	cout << "\n\n\n\n\n\n\n\n\n\t\t\t\t==========================================================\n";
+    cout << "\t\t\t\t||\t\t            BIODATA           \t\t||\n";
+    cout << "\t\t\t\t|| Nama  \t\t\t: Iqbal Maulana Asyari \t||"<<endl;
+    cout << "\t\t\t\t|| Umur \t\t\t: 17 \t\t\t||"<<endl;
+    cout << "\t\t\t\t|| Tanggal Lahir Anda \t\t: \t\t\t||"<<endl;
+    cout << "\t\t\t\t|| NPM \t\t\t\t: \t\t\t||"<<endl;
+    cout << "\t\t\t\t|| Kelas Anda  \t\t\t: \t\t\t||"<<endl;
+    cout << "\t\t\t\t|| Asal Sekolah    \t\t: \t\t\t||"<<endl;
+    cout << "\t\t\t\t==========================================================\n";
+	cin.get();
+	system("cls");
+	cout << "\n\n\n\n\n\n\n\n\n\t\t\t\t==========================================================\n";
+    cout << "\t\t\t\t||\t\t            BIODATA           \t\t||\n";
+    cout << "\t\t\t\t|| Nama  \t\t\t: Iqbal Maulana Asyari \t||"<<endl;
+    cout << "\t\t\t\t|| Umur \t\t\t: 17 \t\t\t||"<<endl;
+    cout << "\t\t\t\t|| Tanggal Lahir Anda \t\t: 17 Feb 2005 \t\t||"<<endl;
+    cout << "\t\t\t\t|| NPM \t\t\t\t: \t\t\t||"<<endl;
+    cout << "\t\t\t\t|| Kelas Anda  \t\t\t: \t\t\t||"<<endl;
+    cout << "\t\t\t\t|| Asal Sekolah    \t\t: \t\t\t||"<<endl;
+    cout << "\t\t\t\t==========================================================\n";
+    cin.get();
+	system("cls");
+	cout << "\n\n\n\n\n\n\n\n\n\t\t\t\t==========================================================\n";
+    cout << "\t\t\t\t||\t\t            BIODATA           \t\t||\n";
+    cout << "\t\t\t\t|| Nama  \t\t\t: Iqbal Maulana Asyari \t||"<<endl;
+    cout << "\t\t\t\t|| Umur \t\t\t: 17 \t\t\t||"<<endl;
+    cout << "\t\t\t\t|| Tanggal Lahir Anda \t\t: 17 Feb 2005 \t\t||"<<endl;
+    cout << "\t\t\t\t|| NPM \t\t\t\t: 5520122019 \t\t||"<<endl;
+    cout << "\t\t\t\t|| Kelas Anda  \t\t\t: \t\t\t||"<<endl;
+    cout << "\t\t\t\t|| Asal Sekolah    \t\t: \t\t\t||"<<endl;
+    cout << "\t\t\t\t==========================================================\n";
+    cin.get();
+	system("cls");
+	cout << "\n\n\n\n\n\n\n\n\n\t\t\t\t==========================================================\n";
+    cout << "\t\t\t\t||\t\t            BIODATA           \t\t||\n";
+    cout << "\t\t\t\t|| Nama  \t\t\t: Iqbal Maulana Asyari \t||"<<endl;
+    cout << "\t\t\t\t|| Umur \t\t\t: 17 \t\t\t||"<<endl;
+    cout << "\t\t\t\t|| Tanggal Lahir Anda \t\t: 17 Feb 2005 \t\t||"<<endl;
+    cout << "\t\t\t\t|| NPM \t\t\t\t: 5520122019 \t\t||"<<endl;
+    cout << "\t\t\t\t|| Kelas Anda  \t\t\t: Informatika B 2022 \t||"<<endl;
+    cout << "\t\t\t\t|| Asal Sekolah    \t\t: \t\t\t||"<<endl;
+    cout << "\t\t\t\t==========================================================\n";
+    cin.get();
+	system("cls");
+	cout << "\n\n\n\n\n\n\n\n\n\t\t\t\t==========================================================\n";
+    cout << "\t\t\t\t||\t\t            BIODATA           \t\t||\n";
+    cout << "\t\t\t\t|| Nama  \t\t\t: Iqbal Maulana Asyari \t||"<<endl;
+    cout << "\t\t\t\t|| Umur \t\t\t: 17 \t\t\t||"<<endl;
+    cout << "\t\t\t\t|| Tanggal Lahir Anda \t\t: 17 Feb 2005 \t\t||"<<endl;
+    cout << "\t\t\t\t|| NPM \t\t\t\t: 5520122019 \t\t||"<<endl;
+    cout << "\t\t\t\t|| Kelas Anda  \t\t\t: Informatika B 2022 \t||"<<endl;
+    cout << "\t\t\t\t|| Asal Sekolah    \t\t: SMKN 1 Cianjur \t||"<<endl;
+    cout << "\t\t\t\t==========================================================\n";
+	cin.get();
+	system("cls");
+	cout<<"\n\n\n\n\n\n\n\n\n\t\t\t\t\t\t========Terima Kasih!=======\n\n";
+	cin.get();
+
+}
